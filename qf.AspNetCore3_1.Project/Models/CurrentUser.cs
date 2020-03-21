0c2984b9a569e5cca7762b73aba98cf534f40bf2
zhq092012 <36983262+zhq092012@users.noreply.github.com>
2020-03-21 18:35:30 +0800

    first

---
+++ qf.AspNetCore3_1.Project/Models/CurrentUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace qf.AspNetCore3_1.Project.Models
+{
+  public class CurrentUser
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Account { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    public string Role { get; set; }
+    public DateTime LoginTime { get; set; }
+  }
+}
---
+++ qf.AspNetCore3_1.Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace qf.AspNetCore3_1.Project.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
