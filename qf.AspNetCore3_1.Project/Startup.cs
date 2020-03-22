580a0261613fb0db6fefad8a11074a102666438b
zhq092012 <36983262+zhq092012@users.noreply.github.com>
2020-03-22 10:45:32 +0800

    添加自定义中间件管道

--- qf.AspNetCore3_1.Project/Startup.cs
+++ qf.AspNetCore3_1.Project/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using log4net.Core;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,35 +33,93 @@ namespace qf.AspNetCore3_1.Project
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
     {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-      else
-      {
-        app.UseExceptionHandler("/Home/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-      }
-      app.UseSession();
-      //注册log4写日志
-      loggerFactory.AddLog4Net();
+      //app.Run(c => c.Response.WriteAsync("hello world."));
+      #region 自定义中间件
+      //Func<RequestDelegate, RequestDelegate> func = new Func<RequestDelegate, RequestDelegate>(
+      //   rd =>
+      //   {
+      //     //return new RequestDelegate(Do);
+      //     //return new RequestDelegate(Do1);
+      //     //return new RequestDelegate(async context => await Task.Run(() => context.Response.WriteAsync("hello world.")));
+      //     //return new RequestDelegate(async context => await Task.Run(() => context.Response.WriteAsync("hello world.")));
+      //     //return async context => await Task.Run(() => context.Response.WriteAsync("hello world."));
+      //     return context => context.Response.WriteAsync("hello world.");
+      //   }
+      //  );
+      //app.Use(rd => { return context => context.Response.WriteAsync("hello world."); });
+      //app.Use(rd => context => context.Response.WriteAsync("hello world."));
 
-      loggerFactory.CreateLogger<Program>().LogWarning("this is config");
+      app.Use(next =>
+     {
+       Console.WriteLine("middleware1");
+       return async c =>
+       {
+         await c.Response.WriteAsync("this is middleware 1 Start<br/>");
+         await c.Response.CompleteAsync();
+         await next.Invoke(c);
+         await c.Response.WriteAsync("this is middleware 1 End<br/>");
+         await c.Response.CompleteAsync();
+       };
 
-      app.UseHttpsRedirection();
-      app.UseStaticFiles();
 
-      app.UseRouting();
+     });
+      //app.Use(next =>
+      //{
+      //  Console.WriteLine("middleware2");
+      //  return async c =>
+      //  {
+      //    await c.Response.WriteAsync("this is middleware 2 Start<br/>");
+      //    await next.Invoke(c);
+      //    await c.Response.WriteAsync("this is middleware 2 End<br/>");
+      //  };
+      //});
+      #endregion
 
-      app.UseAuthorization();
+      //if (env.IsDevelopment())
+      //{
+      //  app.UseDeveloperExceptionPage();
+      //}
+      //else
+      //{
+      //  app.UseExceptionHandler("/Home/Error");
+      //  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+      //  app.UseHsts();
+      //}
+      ////启用session
+      //app.UseSession();
 
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllerRoute(
-                  name: "default",
-                  pattern: "{controller=Home}/{action=Index}/{id?}");
-      });
+      //#region 注册log4写日志
+      ////注册log4写日志
+      //loggerFactory.AddLog4Net();
+
+      //loggerFactory.CreateLogger<Program>().LogWarning("this is config");
+      //#endregion
+
+
+      //app.UseHttpsRedirection();
+      //app.UseStaticFiles();
+
+      //app.UseRouting();
+
+      //app.UseAuthorization();
+
+      //app.UseEndpoints(endpoints =>
+      //{
+      //  endpoints.MapControllerRoute(
+      //            name: "default",
+      //            pattern: "{controller=Home}/{action=Index}/{id?}");
+      //});
     }
+    #region 私有方法
+    //private Task Do(HttpContext context)
+    //{
+    //  return Task.Run(() => { context.Response.WriteAsync("hello world."); });
+    //}
+    //private async Task Do1(HttpContext context)
+    //{
+    //  await Task.Run(() => context.Response.WriteAsync("hello world."));
+    //}
+    #endregion
+
   }
 }
