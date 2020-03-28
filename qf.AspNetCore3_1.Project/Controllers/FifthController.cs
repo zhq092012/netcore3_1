4cfeda114e0c17ce4be80f33172b7725158c36e1
zhq092012 <36983262+zhq092012@users.noreply.github.com>
2020-03-28 10:37:33 +0800

    创建exceptionfilter

---
+++ qf.AspNetCore3_1.Project/Controllers/FifthController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using qf.AspNetCore3_1.Project.Utility;
+
+namespace qf.AspNetCore3_1.Project.Controllers
+{
+    public class FifthController : Controller
+    {
+        [CustomExceptionFilter]
+        public IActionResult Index()
+        {
+            throw new Exception("new excetion");
+            return View();
+        }
+        [CustomExceptionFilter]
+        public IActionResult Info()
+        {
+            throw new Exception("new excetion Info");
+            return View();
+        }
+
+    }
+}
